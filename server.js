952528580674be4bd20091a47092f1531bf4656d
aligamal <aligamal@3ddx.com>
2020-02-10 11:50:25 +0200

    deploying

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var cors = require('cors');
+var http = require ("http");
+var server = http.createServer(app);
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+app.use(express.static("public"));
+// app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.set('view engine', 'ejs');
+
+app.post("/uploadData", (req, res) => {
+  if(req.body.randomDataString)
+   res.status(200).json({msg:"success"})
+  else
+   res.status(400).json({msg:"failed"})
+});
+
+app.get('/downloadData', function(req, res) {
+    res.sendFile(__dirname+"/public/img.jpg");
+});
+
+
+app.get('/', function(req, res) {
+    res.render("./index");
+});
+
+server.listen( process.env.PORT || 5050,()=>{
+    console.log(`server is running on port ${server.address().port}`);
+})
